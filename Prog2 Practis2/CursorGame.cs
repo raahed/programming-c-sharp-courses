75e6cff58e26b1a06a18841a6110c59571b6f89d
Marvin Fette <fettema87760@th-nuernberg.de>
2022-04-28 17:41:03 +0200

    Merge from https://github.com/raahed/study-prog2-practis-2

---
+++ Prog2 Practis2/CursorGame.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace study_prog2_practis_2
+{
+    class CursorGame
+    {
+        private int pointer_left, pointer_top;
+
+        public CursorGame()
+        {
+            setup();
+        }
+
+        private void setup()
+        {
+            Console.CursorVisible = false;
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+
+            Console.BackgroundColor = ConsoleColor.Gray;
+
+            Console.Clear();
+
+            pointer_left = pointer_top = 10;
+        }
+
+        public void play()
+        {
+            while (true)
+            {
+                char key;
+
+                Console.SetCursorPosition(pointer_left, pointer_top);
+
+                Console.Write("X");
+
+                key = Console.ReadKey(true).KeyChar;
+
+                switch (key)
+                {
+                    case 'a':
+                        pointer_left -= 1;
+                        break;
+
+                    case 'd':
+                        pointer_left += 1;
+                        break;
+
+                    case 'w':
+                        pointer_top -= 1;
+                        break;
+
+                    case 's':
+                        pointer_top += 1;
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Prog2 Practis2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading;
+
+namespace study_prog2_practis_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CursorGame cg = new CursorGame();
+            cg.play();
+        }
+    }
+}
