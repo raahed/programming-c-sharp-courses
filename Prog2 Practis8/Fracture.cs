dcb2ede86cfff3febbe7bb8e3f58fc077c00dcd5
Marvin Fette <fettema87760@th-nuernberg.de>
2022-05-12 11:27:27 +0200

    Work on Prog2 Practis8

---
+++ Prog2 Practis8/Fracture.cs
@@ -0,0 +1,63 @@
+﻿namespace Prog2_Practis8
+{
+    public class Fracture : IComparable
+    {
+        private int _numerator;
+        private int _denominator;
+
+        // "Zähler"
+        public int Numerator
+        {
+            get { return _numerator; }
+            set { _numerator = value; }
+        }
+
+        // "Nenner"
+        public int Denominator
+        {
+            get { return _denominator; }
+            set
+            {
+                if (value == 0)
+                    throw new DivideByZeroException();
+
+                _denominator = value;
+            }
+        }
+
+        public Fracture()
+        {
+            Numerator = Denominator = 1;
+        }
+
+        public Fracture(int numerator, int denominator)
+        {
+            Numerator = numerator;
+            Denominator = denominator;
+        }
+
+        public override string ToString()
+        {
+            return $"Bruch: {Numerator}/{Denominator} = {(double)Numerator/Denominator}";
+        }
+
+        int IComparable.CompareTo(object? obj)
+        {
+            // Testing is null or not type of Fracture
+            if (obj is null || obj is not Fracture fracture)
+                throw new NullReferenceException(nameof(obj));
+
+            // If value is equal
+            if (Numerator / Denominator == fracture.Numerator / fracture.Denominator)
+                return 0;
+
+            // If value is smaller
+            if (Numerator / Denominator < fracture.Numerator / fracture.Denominator)
+                return -1;
+
+            // If value is greater
+            else
+                return 1;
+        }
+    }
+}
---
+++ Prog2 Practis8/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Prog2_Practis8
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testing Fracture...");
+
+            Fracture[] fractures = new Fracture[]
+            {
+                new Fracture(12,44),
+                new Fracture(5,6),
+                new Fracture(3,9),
+                new Fracture(7,10),
+                new Fracture(11,5),
+                new Fracture(4,2),
+                new Fracture()
+            };
+
+            Console.WriteLine("Unsorted:");
+            foreach (Fracture fract in fractures)
+                Console.WriteLine(fract);
+
+            Array.Sort(fractures);
+
+            Console.WriteLine("Sorted:");
+            foreach (Fracture fract in fractures)
+                Console.WriteLine(fract);
+        }
+    }
+}
