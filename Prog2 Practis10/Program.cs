768284fb7d150d77075422bcd94e3068a7170783
Marvin Fette <fettemarvin@gmail.com>
2022-05-23 12:05:07 +0200

    Implement Stack

---
+++ Prog2 Practis10/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Prog2_Practis10
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Testing Stack...");
+            Stack<int> stack = new Stack<int>();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+
+            try
+            {
+                Console.WriteLine(stack.Pop());
+                Console.WriteLine(stack.Pop());
+                Console.WriteLine(stack.Pop());
+                Console.WriteLine(stack.Pop());
+                Console.WriteLine(stack.Pop());
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine($"One Operation was outside of boundary: {e.Message}");
+            }
+
+
+        }
+    }
+}
---
+++ Prog2 Practis10/Stack.cs
@@ -0,0 +1,62 @@
+﻿namespace Prog2_Practis10
+{
+    public class Stack<T>
+    {
+        private class Element<T>
+        {
+            public Element<T> next = null;
+            public Element<T> prev = null;
+            public T value;
+
+            public Element(T value)
+            {
+                this.value = value;
+            }
+        }
+
+        Element<T> bottom = null;
+
+        public Stack() { }
+
+        public T Pop()
+        {
+            Element<T> element = bottom;
+
+            if (element == null)
+                throw new IndexOutOfRangeException("Empty Stack");
+
+            while (element.next != null)
+                element = element.next;
+
+            if(element.prev != null)
+            {
+                element.prev.next = null;
+                element.prev = null;
+            } else
+            {
+                bottom = null;
+            }
+
+            return element.value;
+        }
+
+        public void Push(T value)
+        {
+            Element<T> newElement = new Element<T>(value);
+
+            if (bottom == null)
+            {
+                bottom = newElement;
+                return;
+            }
+
+            Element<T> element = bottom;
+
+            while (element.next != null)
+                element = element.next;
+
+            element.next = newElement;
+            newElement.prev = element;
+        }
+    }
+}
