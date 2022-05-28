490018bc9cc875784b1c2478cef3cf6acb357b49
Marvin Fette <fettemarvin@gmail.com>
2022-05-28 21:18:35 +0200

    Work on Prog2 Practis11

---
+++ Prog2 Practis11/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Prog2_Practis11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<int> list = new List<int>();
+            list.Add(-3);
+            list.Add(-2);
+            list.Add(-1);
+            list.Add(0);
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+
+            Console.WriteLine("Testing Filter für Positive Zahlen...");
+
+
+        }
+
+        static void Filter(List<T> list, ) where T : struct
+        {
+
+        }
+
+
+    }
+} Q
