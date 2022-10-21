81bd65455d66e2df195cda341279d2cf0dd86c57
Marvin Fette <fettemarvin@gmail.com>
2022-10-21 16:04:56 +0200

    Start to work on Prog3 Task3

---
+++ Prog3 Practis3.1/ModelStundent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prog3_Practis3._1
+{
+    public class ModelStundent
+    {
+        int matNr;
+
+        string name;
+    }
+}
---
+++ Prog3 Practis3.2/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace DataBinding
+{
+    /// <summary>
+    /// Code behind of MainWindow.xaml
+    /// 
+    /// The code of this particular project is intentionally left incomplete
+	/// (code-behind as well as XAML). Missing pieces of code are indicated by
+	/// markers such as ___1___.
+	///
+    /// As an exercise, the missing pieces of code should be identified and
+	/// inserted. Do not add additional code. The full program should use data
+	/// bindings in order to display 9876543 in each Label once the button
+    /// Change is clicked.
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // Create an instance of class student
+        protected MyClass myObject = new MyClass();
+        public MyClass MyObject
+        {
+            get { return myObject; }
+            set { myObject = value; }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Binding myBinding = new Binding(___1___);
+            myBinding.Source = ___2___;
+            A.SetBinding(___3___, myBinding);
+
+            D.DataContext = ___4___;
+        }
+
+        private void Change(object sender, RoutedEventArgs e)
+        {
+            myObject.MyField = 9876543;
+        }
+    }
+
+    /// <summary>
+    /// A simple representation of a student's properties.
+    /// </summary>
+    public class MyClass : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Constructor, creates an empty object.
+        /// </summary>
+        public MyClass()
+        {
+        }
+
+        public event PropertyChangedEventHandler? ___12___;
+
+        /// <summary>
+        /// Raise a PropertyChanged event.
+		///
+		/// Should be called after a value change in one of this' properties. 
+        /// </summary>
+        /// <param name="pname">A string describing the name of the property that has changed.</param>
+        protected void OnPropertyChanged(string pname)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(pname));
+            }
+        }
+
+        int _MyField = 708312;
+        public int MyField
+        {
+            set
+            {
+                _MyField = value;
+                OnPropertyChanged(___11___);
+            }
+            get
+            {
+                return _MyField;
+            }
+        }
+    }
+}
