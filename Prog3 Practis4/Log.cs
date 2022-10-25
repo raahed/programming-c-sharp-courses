9dff64c19a164664eec7e13ede6bdf4df7741eb7
Marvin Fette <fettemarvin@gmail.com>
2022-10-25 15:21:44 +0200

    Prepare Prog3 Task4

---
+++ Prog3 Practis4/Log.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+
+namespace FittsExercise
+{
+    class Logger
+    {
+        private int[] startX;
+        private int[] startY;
+        private int[] stopX;
+        private int[] stopY;
+        private uint[] targetW;
+        private uint[] targetH;
+        private uint[] error;
+        private DateTime[] startTime;
+        private DateTime[] stopTime;
+
+        public Logger(uint nbrOfTasks)
+        {
+            this.startTime = new DateTime[nbrOfTasks];
+            this.stopTime = new DateTime[nbrOfTasks];
+            this.startX = new int[nbrOfTasks];
+            this.startY = new int[nbrOfTasks];
+            this.stopX = new int[nbrOfTasks];
+            this.stopY = new int[nbrOfTasks];
+            this.targetW = new uint[nbrOfTasks];
+            this.targetH = new uint[nbrOfTasks];
+            this.error = new uint[nbrOfTasks];
+        }
+        public void BeginLogEntry(uint counter, double x, double y, uint w, uint h)
+        {
+            // Start measurement of time
+            this.startTime[counter] = DateTime.Now;
+            // Record mouse coordinates, calucation happens in HitButtonClicked
+            this.startX[counter] = (int)x;
+            this.startY[counter] = (int)y;
+            // Record width and height
+            this.targetW[counter] = w;
+            this.targetH[counter] = h;
+        }
+        public void EndLogEntry(uint counter, double x, double y, uint taskErrorCount)
+        {
+            // store current time
+            this.stopTime[counter] = DateTime.Now;
+            // calculate distance
+            this.stopX[counter] = (int)x;
+            this.stopY[counter] = (int)y;
+            // store number errors counted so far
+            this.error[counter] = taskErrorCount;
+        }
+        public void Save(string fileName, uint experimentId, bool resetMousePos, bool precuing)
+        {
+            // save document
+            using (StreamWriter file = new StreamWriter(fileName))
+            {
+                // iterate measurements
+                for (int i = 0; i < this.startX.Length; i++)
+                {
+                    // calculate milliseconds of the current measurement
+                    int dt = (stopTime[i] - startTime[i]).Seconds * 1000 + (stopTime[i] - startTime[i]).Milliseconds;
+                    int dx = stopX[i] - startX[i];
+                    int dy = stopY[i] - startY[i];
+                    // write configuration data
+                    file.Write(experimentId + ";" + resetMousePos + ";" + precuing);
+                    // add measurements
+                    file.WriteLine(";" + dt + ";" + dx + ";" + dy + ";" + targetW[i] + ";" + targetH[i] + ";" + error[i].ToString());
+                }
+            }
+        }
+    }
+}
---
+++ Prog3 Practis4/MainWindow.xaml.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace FittsExercise
+{
+    /// <summary>
+    /// Experiment addressing Fitt's law.
+    /// 
+    /// Measures time to acquire ("click") a randomly placed button ("target").
+    /// 
+    /// Precuing (optionally): The target is displayed before a measurement starts. 
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Random myRandomizer;
+        private uint counter = 0;
+
+        private uint experimentId = 1;
+        // number of measurements per experiment
+        private uint nbrOfTasks = 2;
+        // mouse pointer automatically set to start button once target is clicked
+        private bool resetMousePos = false;
+        // display new target once current target is clicked 
+        private bool precuing = true;
+
+        // flag, indicates if the start button was pressed (...and the hit button should be released)
+        private bool doAcquireTarget = false;
+
+        // "click me!" brush
+        readonly Brush brClickMe = Brushes.GreenYellow;
+        readonly Brush brDontClickMe = Brushes.White;
+
+        // number of errors across all tasks of this experiment session
+        private uint sessionErrorCount = 0;
+        // number of errors observed during the current target acquisition task
+        private uint taskErrorCount = 0;
+
+        // Prepare a logger to record target acquisition data (init during construction)
+        Logger? log;
+
+        /// <summary>
+        /// Construct the main window. Initializes all UI elements.
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.myRandomizer = new Random();
+
+            // Compute random id for the upcoming experiment
+            this.experimentId = (uint)this.myRandomizer.Next(1, 10000);
+
+            // Highlight start button
+            this.bnStart.Background = brClickMe;
+
+            // TODO Use a modal setup dialog to configure experimentId, nbrOfTasks, resetMousePos, precuing
+
+            // Execute if setup was confirmed
+            log = new Logger(nbrOfTasks);
+            Loaded += delegate (object sender, RoutedEventArgs e) { CreateTarget(); };
+
+            // TODO Execute if setup was canceled
+            // Close();
+        }
+
+        /// <summary>
+        /// Repositions and resizes target.
+        /// 
+        /// Repositioning is continued until a valid position is found - e.g., one not
+        /// hiding the start button.
+        /// </summary>
+        public void CreateTarget()
+        {
+            // Hide target if precuing is disabled
+            if (!this.precuing) this.Target.Visibility = Visibility.Hidden;
+
+            // Target of random size
+            this.Target.Width = this.Target.Height = this.myRandomizer.Next(5, 100);
+            do
+            {
+                // Assign a random position to target
+                this.Target.Margin = new Thickness(this.myRandomizer.Next(0, 500), this.myRandomizer.Next(0, 500), 0, 0);
+                // Ensure that margins and sizes are up-to-date
+                UpdateLayout();
+            } while (TargetPositionInvalid());
+        }
+
+        /// <summary>
+        /// Validates the target's current position.
+        /// 
+        /// Used for positioning the target at the beginning of an experiment, and for collision
+        /// detection in case of animated targets.
+        /// </summary>
+        /// <returns></returns>
+        private bool TargetPositionInvalid()
+        {
+            Rect rTarget = BoundsRelativeTo(Target, Canvas);
+            Rect rStart = BoundsRelativeTo(bnStart, Canvas);
+            return rTarget.IntersectsWith(rStart);
+        }
+        public static Rect BoundsRelativeTo(FrameworkElement element, Visual relativeTo)
+        {
+            return element.TransformToVisual(relativeTo).TransformBounds(new Rect(element.RenderSize));
+        }
+        public Point GetCenter(FrameworkElement el)
+        {
+            return new Point(el.Margin.Left + el.ActualWidth / 2, el.Margin.Top + el.ActualHeight / 2);
+        }
+
+        private void OnStartClicked(object sender, RoutedEventArgs e)
+        {
+            if (this.doAcquireTarget)
+            {
+                // start was clicked, but the user should have clicked target
+                this.taskErrorCount++;
+            }
+            else
+            {
+                // Start counting errors
+                this.taskErrorCount = 0;
+
+                // Record target acquisition setup
+                this.log?.BeginLogEntry(counter, Mouse.GetPosition(this).X, Mouse.GetPosition(this).Y, (uint)this.Target.Width, (uint)this.Target.Height);
+
+                // Unlock the target button
+                this.doAcquireTarget = true;
+                this.bnStart.Background = this.brDontClickMe;
+                this.Target.Fill = this.brClickMe;
+                this.Target.Visibility = Visibility.Visible;
+            }
+            // prevent error count to be increased by window mouseLeftButtonDown handler
+            e.Handled = true;
+        }
+
+        private void OnTargetClicked(object sender, MouseButtonEventArgs e)
+        {
+            if (this.doAcquireTarget)
+            {
+                // enforce need to click start button
+                this.doAcquireTarget = false;
+
+                // Update overall error count
+                this.sessionErrorCount += this.taskErrorCount;
+
+                // Finalize target acquisition setup
+                log?.EndLogEntry(counter, Mouse.GetPosition(this).X, Mouse.GetPosition(this).Y, taskErrorCount);
+
+                // decide on what to do in the next iteration
+                this.counter++;
+                if (this.counter < this.nbrOfTasks)
+                {
+                    CreateTarget();
+
+                    // highlight the button to be clicked next
+                    bnStart.Background = brClickMe;
+                    Target.Fill = brDontClickMe;
+
+                    if (resetMousePos) SetMouseCursorPos(MainWindowInstance.Width / 2, MainWindowInstance.Height / 2);
+                }
+                else
+                {
+                    // experiment accomplished
+                    this.EndExperiment();
+                }
+            }
+            // prevent error count to be increased by window mouseLeftButtonDown handler
+            e.Handled = true;
+        }
+
+        private void WindowClicked(object sender, MouseButtonEventArgs e)
+        {
+            // Clicked on window instead of target
+            if (this.doAcquireTarget)
+            {
+                this.taskErrorCount++;
+            }
+        }
+
+        private void SetMouseCursorPos(double x, double y)
+        {
+            Point pUpperLeftCorner = new Point(this.Left + x, this.Top + y);
+            // Compute device-dependant (pixel) coordinates from device-independant coordinates
+            Point pPixelCoordinates = PresentationSource.FromVisual(this).CompositionTarget.TransformToDevice.Transform(pUpperLeftCorner);
+            // Move mouse cursor
+            NativeMethods.SetCursorPos((int)pPixelCoordinates.X, (int)pPixelCoordinates.Y);
+        }
+
+        protected void EndExperiment()
+        {
+            this.bnStart.Visibility = Visibility.Hidden;
+            this.Target.Visibility = Visibility.Hidden;
+
+            // TODO Open a message box, which displays the number of errors occurred in this experiment
+
+            // Record this session's data in a file (name includes session id)
+            string fileName = "testResults_" + resetMousePos + "_" + precuing + "_" + experimentId;
+            string fileNameExt = ".csv";
+
+            // TODO Use fileName and fileNameExt to configure a save dialog, which allows for confirming or editing the filename (default folder is ./bin/debug)
+
+            fileName += fileNameExt;
+
+            // Process dialog result
+            if (fileName != null)
+            {
+                log?.Save(fileName, experimentId, resetMousePos, precuing);
+            }
+
+            // Close the application
+            Close();
+        }
+    }
+
+    internal static class NativeMethods
+    {
+        // DLL für API-Funktion importieren; externe Methode SetCursorPos deklarieren; eigene Methode setMouseCursorPos definieren; 
+        [DllImport("user32.dll")]
+        internal static extern int SetCursorPos(int x, int y);
+    }
+}
