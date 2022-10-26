c0c987e55e67e695ff6bd7fb05ac84bc89804c62
Marvin Fette <fettema87760@th-nuernberg.de>
2022-10-26 11:05:35 +0200

    Work on Prog3 Practis3

--- Prog3 Practis3.1/ModelStundent.cs
+++ Prog3 Practis3.1/ModelStundent.cs
@@ -3,13 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel;
 
 namespace Prog3_Practis3._1
 {
-    public class ModelStundent
+    public class ModelStundent : INotifyPropertyChanged
     {
-        int matNr;
+        private int matNr;
 
-        string name;
+        private string name;
+
+        public int MatNumber
+        {
+            set
+            {
+                matNr = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventHandler(nameof(MatNumber));
+            }
+            get
+            {
+                return matNr;
+            }
+        }
+
+        public string Name
+        {
+            set
+            {
+                name = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventHandler(nameof(Name)));
+            }
+            get { return name; }
+        }
+
+
+        public ModelStundent(int mat, string name)
+        {
+            this.matNr = mat;
+            this.name = name;
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
     }
 }
